35a77e0201d666b5b20f82ec0628cbf1905ea8b0
Uladzimir Mikhaliuk <uladzimir.mikhaliuk@yellow.systems>
2020-11-14 11:55:11 +0300

    Added map screen

---
+++ app/src/main/java/com/example/mobile/MapActivity.kt
@@ -0,0 +1,78 @@
+package com.example.mobile
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+
+
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+    private var mapView: MapView? = null
+    private var gmap: GoogleMap? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+
+        var mapViewBundle: Bundle? = null
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAP_VIEW_BUNDLE_KEY)
+        }
+        mapView = findViewById(R.id.mapView)
+        mapView!!.onCreate(mapViewBundle)
+        mapView!!.getMapAsync(this)
+    }
+
+    public override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        var mapViewBundle = outState.getBundle(MAP_VIEW_BUNDLE_KEY)
+        if (mapViewBundle == null) {
+            mapViewBundle = Bundle()
+            outState.putBundle(MAP_VIEW_BUNDLE_KEY, mapViewBundle)
+        }
+        mapView!!.onSaveInstanceState(mapViewBundle)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView!!.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView!!.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView!!.onStop()
+    }
+
+    override fun onPause() {
+        mapView!!.onPause()
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        mapView!!.onDestroy()
+        super.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView!!.onLowMemory()
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        gmap = googleMap
+        gmap!!.setMinZoomPreference(12.5f)
+        val ny = LatLng(53.901028, 27.567705)
+        gmap!!.moveCamera(CameraUpdateFactory.newLatLng(ny))
+    }
+
+    companion object {
+        private const val MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey"
+    }
+}
