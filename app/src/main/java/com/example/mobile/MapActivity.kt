d2f6176fd1eff77d6c363dbb217b482b2a29c313
Uladzimir Mikhaliuk <uladzimir.mikhaliuk@yellow.systems>
2020-11-14 12:10:30 +0300

    Added markers at map

--- app/src/main/java/com/example/mobile/MapActivity.kt
+++ app/src/main/java/com/example/mobile/MapActivity.kt
@@ -6,12 +6,23 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_map.*
 
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
-    private var mapView: MapView? = null
     private var gmap: GoogleMap? = null
+    private val restaraunts
+            = listOf(
+                LatLng(53.901028, 27.567705) to "Restaraunt1",
+                LatLng(53.877542, 27.581999) to "Restaraunt2",
+                LatLng(53.872497, 27.473768) to "Restaraunt3",
+                LatLng(53.908097, 27.436013) to "Restaraunt4",
+                LatLng(53.951222, 27.535651) to "Restaraunt5",
+                LatLng(53.872102, 27.638550) to "Restaraunt6")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
@@ -20,9 +31,8 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         if (savedInstanceState != null) {
             mapViewBundle = savedInstanceState.getBundle(MAP_VIEW_BUNDLE_KEY)
         }
-        mapView = findViewById(R.id.mapView)
-        mapView!!.onCreate(mapViewBundle)
-        mapView!!.getMapAsync(this)
+        mapView.onCreate(mapViewBundle)
+        mapView.getMapAsync(this)
     }
 
     public override fun onSaveInstanceState(outState: Bundle) {
@@ -32,44 +42,57 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
             mapViewBundle = Bundle()
             outState.putBundle(MAP_VIEW_BUNDLE_KEY, mapViewBundle)
         }
-        mapView!!.onSaveInstanceState(mapViewBundle)
+        mapView.onSaveInstanceState(mapViewBundle)
     }
 
     override fun onResume() {
         super.onResume()
-        mapView!!.onResume()
+        mapView.onResume()
     }
 
     override fun onStart() {
         super.onStart()
-        mapView!!.onStart()
+        mapView.onStart()
     }
 
     override fun onStop() {
         super.onStop()
-        mapView!!.onStop()
+        mapView.onStop()
     }
 
     override fun onPause() {
-        mapView!!.onPause()
+        mapView.onPause()
         super.onPause()
     }
 
     override fun onDestroy() {
-        mapView!!.onDestroy()
+        mapView.onDestroy()
         super.onDestroy()
     }
 
     override fun onLowMemory() {
         super.onLowMemory()
-        mapView!!.onLowMemory()
+        mapView.onLowMemory()
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         gmap = googleMap
-        gmap!!.setMinZoomPreference(12.5f)
+        gmap!!.setMinZoomPreference(10.5f)
         val ny = LatLng(53.901028, 27.567705)
         gmap!!.moveCamera(CameraUpdateFactory.newLatLng(ny))
+
+        for(item in restaraunts) {
+            gmap!!.addMarker(
+                MarkerOptions()
+                    .position(item.first)
+                    .title(item.second)
+                    .icon(
+                        BitmapDescriptorFactory
+                            .defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+            )
+        }
+
+
     }
 
     companion object {
